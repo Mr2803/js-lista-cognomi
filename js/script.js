2379921538e482024fd4b4937266132d348437e4
Mr2803 <rispoli.mar2803@gmail.com>
2019-10-23 16:33:46 +0200

    fix script

--- js/script.js
+++ js/script.js
@@ -8,26 +8,49 @@ QUINDI OUTPUT sarà:
 -  posizione in cui si trova il cognome dell’utente nell’array ordinato.
 */
 
+//imposto il mio array
 var cognomi = ["rispoli","fierro","mancini","salvi","cinelli"]
-var cognomeUser;
-document.getElementById('my_list-old').innerHTML =  cognomi ;
-console.log("questà è la lista attuale :" + cognomi)
-// Chiedi all’utente il cognome
-cognomeUser= prompt("inserisci un cognome")
-cognomi.push(cognomeUser);
-console.log("il cognome da te inserito è:" + cognomeUser)
-console.log("la nuova lista è :" + cognomi)
 
-//e stampa la lista ordinata alfabeticamente.
+//imposto la mia var
+var cognomeUser;
+
+//Stampo in pagina il mio array
+document.getElementById('my_list-old').innerHTML =  cognomi ;
+console.log("questà è la lista attuale :" + cognomi)//debug
+
+  // CHIEDI ALL'UTENTE IL COGNOME
+
+//chiedo all'utente il suo cognome per aggiungerlo in lista
+cognomeUser= prompt("inserisci un cognome")
+document.getElementById('new_element').innerHTML =  cognomeUser;
+//uso il comando push per inserire il cognome inserito all'interno dell'array
+cognomi.push(cognomeUser);
+
+console.log("il cognome da te inserito è:" + cognomeUser)//debug
+console.log("la nuova lista è :" + cognomi)//debug
+
+//E STAMPA LA LISTA ORDINATA ALFABETICAMENTE
+
+//do il domando sort all'array che lo ordina alfabeticamente in maniera permanente(non avrò più bisogno di scrivere sort)
 cognomi.sort();
+
+//imposto il valore della variabile i per il mio ciclo while
 var i =0;
 
+//imposto la condizione affinchè il ciclo si ripeta
 while (i < cognomi.length){
-  document.getElementById('my_list-new').innerHTML += cognomi[i]+ ",";
-  console.log(cognomi[i])
+  document.getElementById('my_list-new').innerHTML += cognomi[i]+ " ";
+  console.log(cognomi[i])//debug
+
+  //imposto la mia via di uscita dal ciclo in questo caso la var i avanzerà di 1 fino a diventare >= di cognomi.length
   i++;
 }
-console.log("la nuova lista ordinata alfabeticamente è:" + cognomi.sort());
-document.getElementById('my_position').innerHTML = "la posizione del nuovo elemento nell'array è :" + cognomi.indexOf(cognomeUser);
-console.log("la posizione del nuovo elemento nell'array è :"+ cognomi.indexOf(cognomeUser))
-document.getElementById('my_position2').innerHTML = "la posizione per l'uomo è " + parseInt(cognomi.indexOf(cognomeUser)+1);
+console.log("la nuova lista ordinata alfabeticamente è:" + cognomi);//debug
+
+//Stampo in pagina la posizione del cognome inserito nell'array
+document.getElementById('my_position').innerHTML =  cognomi.indexOf(cognomeUser);
+
+console.log("la posizione del nuovo elemento nell'array è :"+ cognomi.indexOf(cognomeUser))//debug
+
+//Stampo in pagina la posizione del cognome inserito per gli umani , ho utilizzato parseInt per convertire la stringa in numero
+document.getElementById('my_position2').innerHTML = parseInt(cognomi.indexOf(cognomeUser)+1);
